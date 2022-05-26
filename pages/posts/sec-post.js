174892bd50ee258f14f1465649158649bd0025d8
divansh <divanshmahajan09@gmail.com>
2022-05-26 14:49:44 +0530

    local changes

---
+++ pages/posts/sec-post.js
@@ -0,0 +1,12 @@
+
+export default function SecondPost() {
+    return (
+    <>
+    <h1>First Post By Divansh Mahajan</h1>
+    
+    <a href="/">Go to home page by Divansh </a>
+    
+    </>
+  )
+  }
+  
