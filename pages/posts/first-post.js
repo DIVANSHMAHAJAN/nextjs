09c4ad6fa5a3c5ded9f4e26ce786503718696e64
divansh <divanshmahajan09@gmail.com>
2022-05-26 14:55:56 +0530

    head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,10 @@
-
+import Head from 'next/head'
 export default function FirstPost() {
   return (
   <>
+  <Head>
+        <title>First Post</title>
+      </Head>
   <h1>First Post By Divansh Mahajan</h1>
   
   <a href="/">Go to home page by Divansh </a>
